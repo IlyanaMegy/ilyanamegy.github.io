dfdaac7aa7502fa394f998046e5f485ee6194411
Ilyana Megy <ilyanamegy7@gmail.com>
2025-12-19 23:16:39 +0100

    :heavy_plus_sign::globe_with_meridians: Added english version <3

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,24 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
 function App() {
   return (
-    <div className="bg-bg text-text font-sans scroll-smooth">
-      <Navbar />
-      <main>
-        <Hero />
-        <About />
-        <Timeline />
-        <Skills />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+    <LanguageProvider>
+      <div className="bg-bg text-text font-sans scroll-smooth min-w-[585px]">
+        <Navbar />
+        <main>
+          <Hero />
+          <About />
+          <Timeline />
+          <Skills />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </LanguageProvider>
   );
 }
 
