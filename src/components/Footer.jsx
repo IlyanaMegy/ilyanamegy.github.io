642314ec7b42dd9e1ae26d3eb8c1835ffa592c35
Ilyana Megy <ilyanamegy7@gmail.com>
2026-04-09 19:17:39 +0200

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-bg/90 py-6 text-center text-muted mt-12 border-t border-beige/40">
-      © {new Date().getFullYear()} Ilyana Megy
-    </footer>
-  );
-};
-
-export default Footer;
